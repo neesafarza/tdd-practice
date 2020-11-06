015338897936d8edd3fbf52c40ea0623fdec4bf4
neesafarza <aneesazafri@gmail.com>
2020-11-06 16:11:55 +0000

    completed testing course

--- src/db.test.js
+++ src/db.test.js
@@ -3,6 +3,9 @@ import { getUserByUsername } from "./db";
 import { expect } from "chai";
 import { getDatabaseData, resetDatabase, setDatabaseData } from "./test-helpers";
 describe("getUserByUsername", () => {
+  afterEach('reset the database', async () => {
+    await resetDatabase();
+  })
   it("get the correct user from the database given the user", async () => {
 
     const fakeData = [
@@ -22,8 +25,6 @@ describe("getUserByUsername", () => {
 
     const actual = await getUserByUsername("abc");
     const finalDBState = await getDatabaseData('users')
-    await resetDatabase();
-
 
     const expected = {
       id: "123",
@@ -33,5 +34,18 @@ describe("getUserByUsername", () => {
 
     expect(actual).excludingEvery('_id').to.deep.equal(expected);
     expect(finalDBState).excludingEvery('_id').to.deep.equal(fakeData);
+  });
+
+  it('returns null when the user not found', async () => {
+
+    await setDatabaseData('users', [{
+      id: '999',
+      username: 'XYZ',
+      email: 'nobody@gmail.com'
+    }]);
+
+    const actual = await getUserByUsername('def')
+
+    expect(actual).to.be.null;
   });
 });
